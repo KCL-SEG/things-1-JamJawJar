ae08aa353ac1fb32b70a3e7faa76c5255cea57c4
Elizabeth Worthington <jamjawjar@gmail.com>
2022-10-25 10:27:04 +0100

    Add restraints to the Things model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, unique=False, blank=True)
+    quantity = models.PositiveIntegerField(unique=False, validators=[
+        MinValueValidator(0),
+        MaxValueValidator(100)
+    ])
